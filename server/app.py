5c49bc2b19dd32ba8ce5ca6f10972821a4bff352
Tryston <trystonproffit@gmail.com>
2026-03-26 18:28:19 -0400

    Finished

--- server/app.py
+++ server/app.py
@@ -1,1 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+@app.route('/')
+def index():
+    return "Welcome to Flatiron Cars"
+
+@app.route('/<model>')
+def show_model(model):
+    if model in existing_models:
+        return f"Flatiron {model} is in our fleet!"
+    else:
+        return f"No models called {model} exists in our catalog"
